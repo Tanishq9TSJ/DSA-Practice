2ff257e2fb6498f7d312c1fdf0646aed1eed35fb
Tanishq Jadhav <92173241+Tanishq9TSJ@users.noreply.github.com>
2023-05-20 22:26:05 +0530

    Two Pointer questions

---
+++ Day-05(Arrays)/ElementinArray.cpp
@@ -0,0 +1,36 @@
+//Given Q queries, check if the given number is present 
+//in the given array or not.
+//Note: Value of all the elements in the array is less
+//than 10 to power 5
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(int i = 0;i < n; i++){
+        cin >> v[i];
+    }
+
+    const int N = 1e5 + 10;
+    vector<int> freq(N,0);
+    for(int i =0;i<n;i++)
+    {
+        freq[v[i]]++;
+    }
+
+    cout << "Enter queries: ";
+    int q;
+    cin >> q;
+
+    while(q--){
+        int queryelement;
+        cin >> queryelement;
+        cout << freq[queryelement] << endl;
+    }
+}
