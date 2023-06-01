220000571928fb67bc5dd7e36f7aaccc3ed0af23
Tanishq Jadhav <92173241+Tanishq9TSJ@users.noreply.github.com>
2023-06-01 23:12:48 +0530

    Pascal Triangle

---
+++ Day-17(2D-Arrays)/2DVector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v1 = {1,2,3};
+    vector<int> v2 = {4,5,6};
+    vector<int> v3 = {7,8,9};
+    vector<vector<int>> v = {v1,v2,v3};
+    
+    return 0;    
+}
