06812f99a96c38738b4895540877521ee198e22b
Tanishq Jadhav <92173241+Tanishq9TSJ@users.noreply.github.com>
2023-06-13 23:56:45 +0530

    selection sort

---
+++ Day-29(Selection Sort)/SelectionSort.cpp
@@ -0,0 +1,36 @@
+//selection sort
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void SelectionSort(vector<int> &v){
+    int n = v.size();
+
+    for(int i=0;i<n-1;i++){
+        int min_idx = i;
+        for(int j=i+1;j<n;j++){
+            if(v[j]<v[min_idx]){
+                min_idx = j;
+            }
+            swap(v[i],v[min_idx]);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+
+    SelectionSort(v);
+
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
