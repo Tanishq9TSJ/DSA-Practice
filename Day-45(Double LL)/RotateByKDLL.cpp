2fc0e1b3644a9b667f5558f05d28692301ad882b
Tanishq Jadhav <92173241+Tanishq9TSJ@users.noreply.github.com>
2023-06-29 23:42:36 +0530

    rotate the dll

---
+++ Day-45(Double LL)/RotateByKDLL.cpp
@@ -0,0 +1,169 @@
+//Rotate the double linked list by k
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int data){
+        val = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+class DoubleLinkedList{
+    public:
+    Node* head;
+    Node* tail;
+
+    DoubleLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void display(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->val<<"<->";
+            temp = temp->next;
+        }cout<<endl;
+    }
+
+    void insertAtStart(int val){
+        Node* new_node = new Node(val);
+        if(head == NULL){
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        new_node->next = head;
+        head->prev = new_node;
+        head = new_node;
+        return;
+    }
+
+    void insertAtEnd(int val){
+        Node* new_node = new Node(val);
+        if(tail==NULL){
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        tail->next = new_node;
+        new_node->prev = tail;
+        tail = new_node;
+        return;
+    }
+
+    void insertAtPosition(int val, int k){
+        //assuming k is less or equal to length of double linkedlist
+        Node* temp = head;
+        int count = 1;
+        while(count<(k-1)){
+            temp=temp->next;
+            count++;
+        }
+
+        //temp will be pointing to the node at (k-1) position
+        Node* new_node = new Node(val);
+        new_node->next = temp->next;
+        temp->next = new_node;
+
+        new_node->prev = temp;
+        new_node->next->prev = new_node;
+        return;
+    }
+
+    void deleteAtStart(){
+        if(head == NULL){
+            return;
+        }
+
+        Node* temp = head;
+        head = head->next;
+        if(head == NULL){
+            tail = NULL;
+        }else{
+            head->prev = NULL;
+        }
+        free(temp);
+        return;
+    }
+
+    void deleteAtEnd(){
+        if(head==NULL){
+            return;
+        }
+
+        Node* temp = tail;
+        tail = tail->prev;
+        if(tail==NULL){
+            head = NULL;
+        }else{
+            tail->next = NULL;
+        }
+        free(temp);
+        return;
+    }
+
+    void deleteAtPosition(int k){
+
+        //assuming k is less than or equal to length of dll
+        Node* temp = head;
+        int counter = 1;
+        while(counter < k){
+            temp = temp->next;
+            counter++;
+        }
+
+        //now temp is pointing to node at kth position
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+    }
+};
+
+void rotateByN(Node* &head, int pos)
+{
+    if(pos==0){
+        return;
+    } 
+  
+    Node* temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+    head->prev=temp;
+  
+    int count=1;
+    while(count<=pos)
+    {
+        head=head->next;
+        temp=temp->next;
+        count++;
+    }
+  
+    temp->next=NULL;
+    head->prev=NULL;
+}
+
+int main(){
+    DoubleLinkedList dll;
+    dll.insertAtStart(1);
+    dll.insertAtStart(2);
+    dll.insertAtStart(3);
+    dll.insertAtEnd(7);
+    dll.insertAtEnd(9);
+    dll.display();
+    rotateByN(dll.head, 3);
+    dll.display();
+    return 0;
+}
